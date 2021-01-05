44f613d3d7524ef1d25096e3cfba762eeaf43233
Leszek Czechak <leszekczechak@gmail.com>
2021-01-05 23:04:39 +0100

    local MySql database added CrossOrigin for Product configured for Angular

--- src/main/java/pl/czechak/leszek/mygamesshopbackend/repository/ProductRepository.java
+++ src/main/java/pl/czechak/leszek/mygamesshopbackend/repository/ProductRepository.java
@@ -2,8 +2,10 @@ package pl.czechak.leszek.mygamesshopbackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.czechak.leszek.mygamesshopbackend.model.Product;
 
 @Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
