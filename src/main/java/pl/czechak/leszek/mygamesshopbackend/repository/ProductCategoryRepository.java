369d41d7368543a4bb25943d3c1a7f934893040f
Leszek Czechak <leszekczechak@gmail.com>
2021-01-05 23:06:26 +0100

    CrossOrigin for ProductCategory configured for Angular

--- src/main/java/pl/czechak/leszek/mygamesshopbackend/repository/ProductCategoryRepository.java
+++ src/main/java/pl/czechak/leszek/mygamesshopbackend/repository/ProductCategoryRepository.java
@@ -3,9 +3,11 @@ package pl.czechak.leszek.mygamesshopbackend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.czechak.leszek.mygamesshopbackend.model.ProductCategory;
 
 @Repository
 @RepositoryRestResource(collectionResourceRel = "productCategory",path = "product-category")
+@CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
