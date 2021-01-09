e5f9c6a529cfa574f15df488eb22bf3e5b0fe922
Leszek Czechak <leszekczechak@gmail.com>
2021-01-09 17:06:48 +0100

    Exposed id of ProductEntity to endpoints

--- src/main/java/pl/czechak/leszek/mygamesshopbackend/configuration/MyDataRestConfig.java
+++ src/main/java/pl/czechak/leszek/mygamesshopbackend/configuration/MyDataRestConfig.java
@@ -8,8 +8,53 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import pl.czechak.leszek.mygamesshopbackend.model.Product;
 import pl.czechak.leszek.mygamesshopbackend.model.ProductCategory;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private final EntityManager entityManager;
+
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] theUnsuportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsuportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsuportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsuportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsuportedActions));
+
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        List<Class> entityClasses = new ArrayList<>();
+
+        for (EntityType tempEntityType : entities){
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
+}
+
 
 //    @Override
 //    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
@@ -26,21 +71,3 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 //                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsuportedActions))
 //            .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsuportedActions));
 //    }
-
-    @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-        HttpMethod[] theUnsuportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
-
-
-        config.getExposureConfiguration()
-                .forDomainType(Product.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsuportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsuportedActions));
-
-        config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsuportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsuportedActions));
-    }
-}
-
